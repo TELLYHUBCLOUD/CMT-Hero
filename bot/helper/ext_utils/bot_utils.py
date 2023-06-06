31786e06c2c950fd6fda7594e7c9970e5b2ea8d0
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-06-06 18:55:06 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -225,6 +225,7 @@ def get_readable_message():
         button = buttons.build_menu(3)    
     msg += f"\n<b>🄰🄽🄰🄺 🄼🄰🅂🄰🄼🄱🄰</b>"
     msg += f"\n<b>TASKS</b>: {tasks}"
+    msg += f" | <b>CPU</b>: <code>{cpu_percent()}%</code>"
     msg += f"\n<b>DISK</b>: <code>{get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
     msg += f" | <b>RAM</b>: <code>{virtual_memory().percent}%</code>"
     msg += f"\n<b>DL</b>: <code>{get_readable_file_size(dl_speed)}/s</code> ▼"
