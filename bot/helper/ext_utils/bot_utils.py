fdedf89a25c7e2dd66cf8e02b48d8101bb13c51e
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-07-23 08:47:35 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -215,10 +215,10 @@ def get_readable_message():
         button = buttons.build_menu(3)
     msg += f"\n<b>🅿🅴🅰  🅼🅰🆂🅰🅼🅱🅰</b>"
     msg += f"\n<b>ACTIVE</b>: <code>{tasks}</code>"
+    msg += f"\n<b>DISK</b>: <code>{get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
     msg += f" | <b>UPTM</b>: <code>{get_readable_time(time() - botStartTime)}</code>"
-    msg += f"\n<b>DISK</b>: <code>{get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"   
-    msg += f"\n<b>⧩</b>: <code>{get_readable_file_size(dl_speed)}/s</code>"
-    msg += f" | <b>◭</b>: <code>{get_readable_file_size(up_speed)}/s</code>"
+    msg += f"\n<b>DL</b>: <code>{get_readable_file_size(dl_speed)}/s</code>⧩"
+    msg += f" | <b>UL</b>: <code>{get_readable_file_size(up_speed)}/s</code>◭"
     remaining_time = 86400 - (time() - botStartTime)
     res_time = '🚸 KAPAN SAJA 🚸' if remaining_time <= 0 else get_readable_time(remaining_time)
     if remaining_time <= 3600:
