4a6467ce232791be5077eb5ac8a062c4029351f0
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-07-30 19:29:40 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,12 +4,12 @@ from bot import CMD_SUFFIX
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = [f'msb{CMD_SUFFIX}', f'p{CMD_SUFFIX}']
-        self.QbMirrorCommand = [f'msbqb{CMD_SUFFIX}', f'pq{CMD_SUFFIX}']
-        self.YtdlCommand = [f'msbytdl{CMD_SUFFIX}', f'yt{CMD_SUFFIX}']
-        self.LeechCommand = [f'msbleech{CMD_SUFFIX}', f'l{CMD_SUFFIX}']
-        self.QbLeechCommand = [f'msbqbleech{CMD_SUFFIX}', f'qbl{CMD_SUFFIX}']
-        self.YtdlLeechCommand = [f'msbytdlleech{CMD_SUFFIX}', f'ytl{CMD_SUFFIX}']
+        self.MirrorCommand = [f'msb{CMD_SUFFIX}', f'pmsb{CMD_SUFFIX}']
+        self.QbMirrorCommand = [f'msbqb{CMD_SUFFIX}', f'pqmsb{CMD_SUFFIX}']
+        self.YtdlCommand = [f'msbytdl{CMD_SUFFIX}', f'ytmsb{CMD_SUFFIX}']
+        self.LeechCommand = [f'msbleech{CMD_SUFFIX}', f'lmsb{CMD_SUFFIX}']
+        self.QbLeechCommand = [f'msbqbleech{CMD_SUFFIX}', f'qblmsb{CMD_SUFFIX}']
+        self.YtdlLeechCommand = [f'msbytdlleech{CMD_SUFFIX}', f'ytlmsb{CMD_SUFFIX}']
         self.CloneCommand = f'msbclone{CMD_SUFFIX}'
         self.CountCommand = f'msbcount{CMD_SUFFIX}'
         self.DeleteCommand = f'peadel{CMD_SUFFIX}'
@@ -17,15 +17,15 @@ class _BotCommands:
         self.CancelAllCommand = [f'msbcancelall{CMD_SUFFIX}', 'cancelallbot']
         self.ListCommand = f'msblist{CMD_SUFFIX}'
         self.SearchCommand = f'msbsearch{CMD_SUFFIX}'
-        self.StatusCommand = [f'msbstatus{CMD_SUFFIX}', 'sall']
+        self.StatusCommand = [f'msbstatus{CMD_SUFFIX}', 'sallmsb']
         self.UsersCommand = f'msbusers{CMD_SUFFIX}'
         self.AuthorizeCommand = f'msbauthorize{CMD_SUFFIX}'
         self.UnAuthorizeCommand = f'msbunauthorize{CMD_SUFFIX}'
         self.AddSudoCommand = f'msbaddsudo{CMD_SUFFIX}'
         self.RmSudoCommand = f'msbrmsudo{CMD_SUFFIX}'
-        self.PingCommand = [f'msbping{CMD_SUFFIX}','p']
+        self.PingCommand = [f'msbping{CMD_SUFFIX}','ping']
         self.RestartCommand = [f'msbrestart{CMD_SUFFIX}', 'restartall']
-        self.StatsCommand = [f'msbstats{CMD_SUFFIX}', 's']
+        self.StatsCommand = [f'msbstats{CMD_SUFFIX}', 'smsb']
         self.HelpCommand = f'msbhelp{CMD_SUFFIX}'
         self.LogCommand = f'msblog{CMD_SUFFIX}'
         self.ShellCommand = f'msbshell{CMD_SUFFIX}'
