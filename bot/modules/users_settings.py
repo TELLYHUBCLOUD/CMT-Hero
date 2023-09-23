5c7e575554f8fed629b3f95762ed1e3143dd11da
Pea Masamba <96761006+NadifMasamba@users.noreply.github.com>
2023-09-23 08:43:58 +0700

    Minor Fix

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -128,7 +128,7 @@ async def update_user_settings(query):
     user_id = query.from_user.id
     tpath = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(tpath):
-        tpath = "https://telegra.ph/file/60a1816cae4e389407ee5.jpg"
+        tpath = "https://telegra.ph/file/2dd694fa7318e79df3423.jpg"
     await query.message.edit_media(
         media=InputMediaPhoto(media=tpath, caption=msg), reply_markup=button)
 
@@ -138,7 +138,7 @@ async def user_settings(_, message):
     user_id = message.from_user.id
     tpath = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(tpath):
-        tpath = "https://telegra.ph/file/60a1816cae4e389407ee5.jpg"
+        tpath = "https://telegra.ph/file/2dd694fa7318e79df3423.jpg"
     usetMsg = await message.reply_photo(tpath, caption=msg, reply_markup=button)
     await auto_delete_message(message, usetMsg)
 
@@ -368,10 +368,10 @@ Timeout: 60 sec
         await query.answer()
         buttons = ButtonMaker()
         if await aiopath.exists(rclone_path):
-            buttons.ibutton("Delete rcl.conf", f"userset {user_id} drcc")
+            buttons.ibutton("Delete Rclone Config", f"userset {user_id} drcc")
         buttons.ibutton("Back", f"userset {user_id} back")
         buttons.ibutton("Close", f"userset {user_id} close")
-        await editMessage(message, 'Send rcl.conf. Timeout: 60 sec', buttons.build_menu(1))
+        await editMessage(message, 'Send Rclone Config. Timeout: 60 sec', buttons.build_menu(1))
         pfunc = partial(add_rclone, pre_event=query)
         await event_handler(client, query, pfunc, document=True)
     elif data[2] == 'drcc':
